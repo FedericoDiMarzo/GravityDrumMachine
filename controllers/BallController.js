ffc76fbbd54f7edaf1fab00bf3bd48dcc12dfdfd
FedericoDiMarzo <mail.federicodimarzo@gmail.com>
2020-02-05 16:07:49 +0100

    Regex Validation for Notes

--- controllers/BallController.js
+++ controllers/BallController.js
@@ -349,11 +349,14 @@ class BallController {
         let shootingStyle = autoUpdatableFields.filter(nodeFilter("shooting-style-select"))[0].value;
 
 
+        // setting the size
         this.editedBall.size = parseFloat(size);
-        this.editedBall.note = note;
-        // this.editedBall.soundModule.setGain(parseFloat(gain));
-        // this.editedBall.soundModule.setDelaySend(parseFloat(delay));
-        // this.editedBall.soundModule.setReverbSend(parseFloat(reverb));
+
+        // validating the note before changes
+        let regex = new RegExp("^[CDEFGAB|cdefgab][1-5]$")
+        if (regex.test(note)) {
+            this.editedBall.note = note;
+        }
 
         // changing shooting style
         if (shootingStyle !== "no-value") {
